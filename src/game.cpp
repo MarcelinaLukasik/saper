32269e97a80110a0e4b07f6a7f3f39a09000a267
mac <mac@MacBook-Air-mac.local>
2024-04-15 19:39:18 +0200

    add timer

--- src/game.cpp
+++ src/game.cpp
@@ -38,8 +38,11 @@ void Game::HandleEvents(int **grid, int **sgrid, int x, int y)
                   break;
 
               case Event::MouseButtonPressed:
-                  if (e.key.code == Mouse::Left) sgrid[x][y]=grid[x][y];
-                  else if (e.key.code == Mouse::Right) sgrid[x][y]=11;
+                  if (x >= 0 && x <= gridSize && y >= 0 && y <= gridSize){
+                     if (e.key.code == Mouse::Left) sgrid[x][y]=grid[x][y];
+                     else if (e.key.code == Mouse::Right) sgrid[x][y]=11;
+                  }
+                 
                   break;               
             }
             
@@ -47,19 +50,36 @@ void Game::HandleEvents(int **grid, int **sgrid, int x, int y)
         app->clear(Color::White);
 }
 
-void Game::Display(int **grid, int **sgrid, Sprite s, int x, int y, int w)
+void Game::Display(int **grid, int **sgrid, Sprite s, int x, int y, int w, std::string timeAsText)
 {
+    
+    sf::Font font;
+    font.loadFromFile("SunnySpellsBasic.ttf");
+    sf::Text text(timeAsText, font, 30);
+    text.setPosition(app->getSize().x*0.45, app->getSize().y - text.getGlobalBounds().height - text.getGlobalBounds().top - 15);
+    text.setFillColor(sf::Color::White);
+
+    // define a 120x50 rectangle
+    sf::RectangleShape rectangle(sf::Vector2f(200, 70));
+    // change the size to 100x100
+    rectangle.setSize(sf::Vector2f(app->getSize().x*0.80, 40));
+    rectangle.setPosition(app->getSize().x*0.08, app->getSize().y - rectangle.getGlobalBounds().height - rectangle.getGlobalBounds().top - 5);
+    sf::Color color(0, 0, 128);
+    rectangle.setFillColor(color);
+
     for (int i=1;i<=10;i++)
          for (int j=1;j<=10;j++)
           {
-              if (x >= 0 && x < gridSize && y >= 0 && y < gridSize)
+              if (x >= 0 && x <= gridSize && y >= 0 && y <= gridSize)
            {
               if (sgrid[x][y]==9) sgrid[i][j]=grid[i][j];
            }
            s.setTextureRect(IntRect(sgrid[i][j]*w,0,w,w));
            s.setPosition(i*w, j*w);
            app->draw(s);
+           
           }
-
-        app->display();
+    app->draw(rectangle);
+    app->draw(text);
+    app->display();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ int main()
     srand(time(0));
     Game game;
 
+
+   
     //TODO move creating arrays to separate method in game class
     int** grid { new int*[12] }; // allocate an array of 12 int pointers — these are our rows
     for (int count { 0 }; count < 12; ++count)
@@ -27,7 +29,7 @@ int main()
     for (int i=1;i<=game.gridSize;i++)
      for (int j=1;j<=game.gridSize;j++)
       {
-        sgrid[i][j]=game.gridSize;
+        sgrid[i][j]=game.startTile;
         if (rand()%5==0)  grid[i][j]=9;
         else grid[i][j]=0;
       }
@@ -48,14 +50,23 @@ int main()
         grid[i][j]=n;
       }
    
+
+    sf::Clock clock;
+        
+
     while (game.CheckIfOpen())
     {
+        
+        sf::Time elapsed = clock.getElapsedTime();
+        float timeElapsed =  elapsed.asSeconds();
+        std::string timeAsText = std::to_string(timeElapsed);
+        std::string formatedTime = timeAsText.substr(0, timeAsText.length() - 5);
         Vector2i pos = game.GetMousePosition();
         int x = pos.x/game.w;
         int y = pos.y/game.w;
 
         game.HandleEvents(grid, sgrid, x, y);        
-        game.Display(grid, sgrid, s, x, y, game.w);
+        game.Display(grid, sgrid, s, x, y, game.w, formatedTime);
     }
 
     // Deallocate memory
